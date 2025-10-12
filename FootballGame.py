91a9c900a6e13026fa2ae0153182883009f6197f
moyo-1234 <folake.abudiore@gmail.com>
2025-10-12 19:06:40 +0100

    Add files via upload

--- FootballGame.py
+++ FootballGame.py
@@ -1,7 +1,11 @@
 import pygame
+pygame.font.init()
+font = pygame.font.SysFont("Comfortaa",30)
 
 HEIGHT = 700
-WIDTH = 700
+WIDTH = 800
+LHealth = 10
+RHealth = 10
 run = True
 Screen = pygame.display.set_mode((WIDTH,HEIGHT))
 
@@ -12,11 +16,18 @@ Lgoal = pygame.image.load("goalL.png")
 bg = pygame.transform.scale(bg,(800,800))
 Rgoal = pygame.transform.scale(Rgoal,(50,60))
 Lgoal = pygame.transform.scale(Lgoal,(50,60))
+Rrect = pygame.Rect(200,300,50,40)
+Lrect = pygame.Rect(700,300,50,40)
 
-def draw():
+def draw(Rrect,Lrect):
     Screen.blit(bg,(0,0))
     Screen.blit(Rgoal,(200,300))
     Screen.blit(Lgoal,(500,300))
+    pygame.draw.rect(Screen,"black",(390,0,17,700))
+    RHealthText = font.render("Health:"+str(RHealth),1,"red")
+    Screen.blit(RHealthText,(50,20))
+    LHealthText = font.render("Health:"+str(LHealth),1,"blue")
+    Screen.blit(LHealthText,(700,20))
     pygame.display.update()
 
 
@@ -26,4 +37,4 @@ while run:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             run = False
-    draw()
+    draw(Rrect,Lrect)
