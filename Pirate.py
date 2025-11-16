72cca0be692efd3330bce83156ce3a7279521930
moyo-1234 <folake.abudiore@gmail.com>
2025-11-16 19:15:39 +0000

    Add files via upload

--- Pirate.py
+++ Pirate.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 WIDTH = 500
 HEIGHT = 500
@@ -6,6 +7,7 @@ run = True
 bg = pygame.image.load("C:/Users/femia/Desktop/python_game_dev/Pro Game dev/PirateShip.jpg")
 bg = pygame.transform.scale(bg,(500,500))
 Screen = pygame.display.set_mode((WIDTH,HEIGHT))
+
 class pira(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -16,6 +18,38 @@ class pira(pygame.sprite.Sprite):
 pira1 = pira()
 allsprites = pygame.sprite.Group()
 allsprites.add(pira1)
+class cannonball(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("C:/Users/femia/Desktop/python_game_dev/Pro Game dev/cannonball.png")
+        self.image = pygame.transform.scale(self.image,(30,30))
+        self.rect = self.image.get_rect()
+
+notmoney = pygame.sprite.Group()
+for i in range(10):
+    cannon1 = cannonball()
+    cannon1.rect.x = random.randint(0,460)
+    cannon1.rect.y = random.randint(0,440)
+    allsprites.add(cannon1)
+    cannonball.add(cannon1)
+
+class money(pygame.sprite.Sprite):
+    def __init__(self,img):       
+        super().__init__()
+        self.image = pygame.image.load(img)
+        self.image = pygame.transform.scale(self.image,(30,30))
+        self.rect = self.image.get_rect()
+
+mone = ["C:/Users/femia/Desktop/python_game_dev/Pro Game dev/ring.png","C:/Users/femia/Desktop/python_game_dev/Pro Game dev/coin.png"]
+mon = pygame.sprite.Group()
+
+for i in range(10):
+    money1 = money(random.choice(mone))
+    money1.rect.x = random.randint(0,460)
+    money1.rect.y = random.randint(0,440)
+    allsprites.add(money1)
+    mon.add(money1)
+
 
 
 
