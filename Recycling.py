b72e2c4a433b81cc5621ef848e22d123e4466408
moyo-1234 <folake.abudiore@gmail.com>
2025-11-16 20:01:55 +0000

    Add files via upload

--- Recycling.py
+++ Recycling.py
@@ -1,12 +1,16 @@
 import pygame
 import random
+import time
 pygame.init()
 WIDTH = 500
 HEIGHT = 500
+font = pygame.font.SysFont("Comfortaa",30)
 run = True
+StartTime = time.time()
 bg = pygame.image.load("C:/Users/femia/Desktop/python_game_dev/Pro Game dev/recbg.png")
 bg = pygame.transform.scale(bg,(500,500))
 Screen = pygame.display.set_mode((WIDTH,HEIGHT))
+Score = 0
 class bin(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -50,7 +54,7 @@ for i in range(50):
 
 
 
-
+clock = pygame.time.Clock()
 
 while run:
     for i in pygame.event.get():
@@ -61,16 +65,36 @@ while run:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
         if bin1.rect.y > 0:
-            bin1.rect.y = bin1.rect.y - 1
+            bin1.rect.y = bin1.rect.y - 2
     if keys[pygame.K_DOWN]:
         if bin1.rect.y < 440:
-            bin1.rect.y = bin1.rect.y + 1
+            bin1.rect.y = bin1.rect.y + 2
     if keys[pygame.K_LEFT]:
         if bin1.rect.x > 0:
-            bin1.rect.x = bin1.rect.x - 1
+            bin1.rect.x = bin1.rect.x - 2
     if keys[pygame.K_RIGHT]:
         if bin1.rect.x < 460:
-            bin1.rect.x = bin1.rect.x + 1
+            bin1.rect.x = bin1.rect.x + 2
+    collideR = pygame.sprite.spritecollide(bin1,recyc,True)
+    collideN = pygame.sprite.spritecollide(bin1,nonrecyc,True)
+    for i in collideR:
+        Score = Score + 1
+    for i in collideN:
+        Score = Score - 2
+    ScoreText = font.render("Score:"+str(Score),1,"red")
+    Screen.blit(ScoreText,(420,30))
+    clock.tick(30)
+    TimeElapsed = time.time() - StartTime
+    if TimeElapsed >= 60:
+        if Score > 20:
+            Win = font.render("You Won",1,"gold")
+            Screen.blit(Win,(230,230))
+        else:
+            Lose = font.render("You Lost",1,"red")
+            Screen.blit(Lose,(230,230))
+    else:
+        Time = font.render("Time Left:"+str(60- int(TimeElapsed)),1,"light blue")
+        Screen.blit(Time,(350,50))
     pygame.display.update()
 
         
